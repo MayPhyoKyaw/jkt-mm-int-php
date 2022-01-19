22f3055a7d3a7b7e558eebd3fce7237e80745b41
MayPhyoKyaw <MayPhyoKyaw@users.noreply.github.com>
2022-01-20 00:55:25 +0630

    gg

--- jktmyanmarint.com/paymentDetail.php
+++ jktmyanmarint.com/paymentDetail.php
@@ -1,9 +1,32 @@
 <?php
 
 if (!isset($_GET["enroll_id"])) {
-  header("location: ../frontend/index.html");
+  header("location: index.html");
 }
-include_once '../../jktmyanmarint.admin.com/auth/hashFunc.php';
+function encrypt_decrypt($action, $string) {
+  /* =================================================
+  * ENCRYPTION-DECRYPTION
+  * =================================================
+  * ENCRYPTION: encrypt_decrypt('encrypt', $string);
+  * DECRYPTION: encrypt_decrypt('decrypt', $string) ;
+  */
+  $output = false;
+  $encrypt_method = "AES-256-CBC";
+  $secret_key = 'JKT-2019-20IT85-MM-JP';
+  $secret_iv = 'JKT-2019-serV1ce-MM-JP';
+  // hash
+  $key = hash('sha256', $secret_key);
+  // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+  $iv = substr(hash('sha256', $secret_iv), 0, 16);
+  if ($action == 'encrypt') {
+      $output = base64_encode(openssl_encrypt($string, $encrypt_method, $key, 0, $iv));
+  } else {
+      if ($action == 'decrypt') {
+          $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+      }
+  }
+  return $output;
+}
 ?>
 
 <!DOCTYPE html>
